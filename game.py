433046a84246e0a472530ea5921a7c6ba442c160
chanidu123 <chanidumadalagama@gmail.com>
2021-07-09 21:25:00 +0530

    Dice-rolling-stimulator 1.0

--- game.py
+++ game.py
@@ -1,43 +1,46 @@
 import random
-number = random.randint(1,6)
-if number == 1:
-     print("-------")
-     print("|     |")
-     print("|  0  |")
-     print("|     |")
-     print("-------")
+print("This is Dice-rolling-stimulator")
+print("Develaped By Wolf ;-)")
+x = "y"
 
-if number == 2:
-     print("-------")
-     print("|     |")
-     print("|  00 |")
-     print("|     |")
-     print("-------")
+while x == "y":
+    number = random.randint(1,6)
 
-if number == 3:
-     print("-------")
-     print("|     |")
-     print("|  00 |")
-     print("|   0 |")
-     print("-------")
-
-if number == 4:
-     print("-------")
-     print("|     |")
-     print("|  00 |")
-     print("|  00 |")
-     print("-------")
-
-if number == 5:
-     print("-------")
-     print("|   0  |")
-     print("|  00  |")
-     print("|  00  |")
-     print("-------")
-
-if number == 6:
-     print("-------")
-     print("|      |")
-     print("| 000  |")
-     print("| 000  |")
-     print("-------")
+    if number == 1:
+        print("----------")
+        print("|        |")
+        print("|    O   |")
+        print("|        |")
+        print("----------")
+    if number == 2:
+        print("----------")
+        print("|        |")
+        print("| O    O |")
+        print("|        |")
+        print("----------")
+    if number == 3:
+        print("----------")
+        print("|    O   |")
+        print("|    O   |")
+        print("|    O   |")
+        print("----------")
+    if number == 4:
+        print("----------")
+        print("| O    O |")
+        print("|        |")
+        print("| O    O |")
+        print("----------")
+    if number == 5:
+        print("----------")
+        print("| O    O |")
+        print("|    O   |")
+        print("| O    O |")
+        print("----------")
+    if number == 6:
+        print("----------")
+        print("| O    O |")
+        print("| O    O |")
+        print("| O    O |")
+        print("----------")
+    x = input("Press y to roll again")
+    

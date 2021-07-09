99ff355934bdd1112df2a7dc64d7e842c4c9d1fb
chanidu123 <chanidumadalagama@gmail.com>
2021-07-09 20:33:50 +0530

    Add If function

--- game.py
+++ game.py
@@ -1,3 +1,43 @@
 import random
 number = random.randint(1,6)
-print(number)
+if number == 1:
+     print("-------")
+     print("|     |")
+     print("|  0  |")
+     print("|     |")
+     print("-------")
+
+if number == 2:
+     print("-------")
+     print("|     |")
+     print("|  00 |")
+     print("|     |")
+     print("-------")
+
+if number == 3:
+     print("-------")
+     print("|     |")
+     print("|  00 |")
+     print("|   0 |")
+     print("-------")
+
+if number == 4:
+     print("-------")
+     print("|     |")
+     print("|  00 |")
+     print("|  00 |")
+     print("-------")
+
+if number == 5:
+     print("-------")
+     print("|   0  |")
+     print("|  00  |")
+     print("|  00  |")
+     print("-------")
+
+if number == 6:
+     print("-------")
+     print("|      |")
+     print("| 000  |")
+     print("| 000  |")
+     print("-------")
